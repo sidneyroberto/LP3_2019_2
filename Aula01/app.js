84fcccd2acfd90d491710081e2e0f81870670dc7
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-08-09 22:43:08 -0400

    Adicionando testes ao projeto Aula01

--- Aula01/app.js
+++ Aula01/app.js
@@ -14,16 +14,52 @@ app.use('/inverter/:str', (req, res) => {
     let str = req.params.str;
     // Inverte a string
     str = str.split('').reverse().join('');
-    res.json(str);
+    res.json({ resultado: str });
 });
 
 app.use('/cpf/:cpf', (req, res) => {
     let cpf = req.params.cpf;
-    // Deixo pra vcs!
-    res.send('Validador de CPF');
+    let digitosIguais = true;
+
+    if (cpf.length < 11) {
+        return res.json({ valido: false });
+    }
+    for (let i = 0; i < cpf.length - 1; i++) {
+        if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+            digitosIguais = false;
+            break;
+        }
+    }
+    if (!digitosIguais) {
+        let numeros = cpf.substring(0, 9);
+        let digitos = cpf.substring(9);
+        let soma = 0;
+
+        for (let i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0)) {
+            return res.json({ valido: false });
+        }
+
+        numeros = cpf.substring(0, 10);
+        soma = 0;
+        for (let i = 11; i > 1; i--) {
+            soma += numeros.charAt(11 - i) * i;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1)) {
+            return res.json({ valido: false });
+        }
+
+        return res.json({ valido: true });
+    }
+    else {
+        return res.json({ valido: false });
+    }
 });
 
-
-app.listen(3000,
-    () => console.log('Servidor iniciado')
-);
+module.exports = app;
