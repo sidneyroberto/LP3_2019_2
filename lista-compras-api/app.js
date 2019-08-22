ebbbfdcd957c84f2ed6981cc68865ea1be9286d9
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-08-22 19:40:06 -0400

    Inicializando o projeto lista-compras-api

---
+++ lista-compras-api/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+/**
+ * Ao utilizar a função cors() sem
+ * nenhum argumento, você estará
+ * liberando o acesso a todos os
+ * serviços para qualquer outra aplicação
+ * que tenha acesso à internet.
+ */
+app.use(cors());
+
+/**
+ * Permite que o nosso app Express
+ * consiga interpretar dados do frontend
+ * em formato JSON.
+ */
+app.use(bodyParser.json());
+
+module.exports = app;
